79d6d89e1fd11962ba56a379da6c060c9f993c82
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2024-12-31 12:04:26 +0100

    chore: update to astro 5.0

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,27 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
 	talks: defineCollection({
-		type: 'content',
-		schema: ({ image }) => z.object({
-			title: z.string(),
-			description: z.string(),
-			talkDate: z.coerce.date(),
-			event: z.object({
-				name: z.string(),
-				dateStart: z.coerce.date(),
-				dateEnd: z.coerce.date(),
+		type: "content",
+		schema: ({ image }) =>
+			z.object({
+				title: z.string(),
+				description: z.string(),
+				talkDate: z.coerce.date(),
+				event: z.object({
+					name: z.string(),
+					dateStart: z.coerce.date(),
+					dateEnd: z.coerce.date(),
+				}),
+				tags: z.array(z.string()),
+				img: image(),
+				imgAlt: z.string().optional(),
 			}),
-			tags: z.array(z.string()),
-			img: image().refine((img) => img.width >= 600, {
-				message: "Talk image must be at least 600 pixels wide!",
-			}),
-			imgAlt: z.string().optional(),
-		}),
 	}),
 	mentions: defineCollection({
-		type: 'content',
+		type: "content",
 		schema: z.object({
-			brand: z.string()
-		})
-	})
+			brand: z.string(),
+		}),
+	}),
 };
