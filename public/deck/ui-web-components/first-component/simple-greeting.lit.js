e6787196b2726fceca0ade03f1654794abb172d6
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2025-05-14 22:50:57 +0200

    refactor: context example as import

--- public/deck/ui-web-components/first-component/simple-greeting.lit.js
+++ public/deck/ui-web-components/first-component/simple-greeting.lit.js
@@ -1,23 +1,23 @@
 import { html, css, LitElement } from 'https://cdn.skypack.dev/lit';
 
 class SimpleGreetingLit extends LitElement {
-    static styles = css`
+	static styles = css`
 		p {
 			color: var(--accent-regular);
 		}
 	`;
 
-    static properties = {
-        name: { type: String },
-    };
+	static properties = {
+		name: { type: String },
+	};
 
-    constructor() {
-        super();
-        this.name = 'Somebody';
-    }
+	constructor() {
+		super();
+		this.name = 'Somebody';
+	}
 
-    render() {
-        return html`<p>Hello, ${this.name}!</p>`;
-    }
+	render() {
+		return html`<p>Hello, ${this.name}!</p>`;
+	}
 }
 customElements.define('simple-greeting-lit', SimpleGreetingLit);
